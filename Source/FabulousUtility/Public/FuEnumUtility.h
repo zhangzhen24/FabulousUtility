2a5ea8c57cc1112750e5291d1485eefc27570be1
Zhang Zhen <735398170@qq.com>
2024-11-23 15:36:52 +0800

    add FuEnumUtility
    add FuEnumUtility

---
+++ Source/FabulousUtility/Public/FuEnumUtility.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+namespace FuEnumUtility
+{
+	template <typename EnumType>
+	DLLEXPORT int32 GetIndexByValue(const EnumType Value)
+	{
+		return StaticEnum<EnumType>()->GetIndexByValue(static_cast<int64>(Value));
+	}
+
+	template <typename EnumType>
+	DLLEXPORT FString GetNameStringByValue(const EnumType Value)
+	{
+		return StaticEnum<EnumType>()->GetNameStringByValue(static_cast<int64>(Value));
+	}
+}
