3df0a8d2048ca70a89e8c1ca6ab95781b7e54245
Zhang Zhen <735398170@qq.com>
2024-11-23 15:11:33 +0800

    Add fabulous editor module
    add fabulous editor module~

---
+++ Source/FabulousEditor/FabulousEditor.Build.cs
@@ -0,0 +1,18 @@
+﻿using UnrealBuildTool;
+
+public class FabulousEditor : ModuleRules
+{
+	public FabulousEditor(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+
+		bEnableNonInlinedGenCppWarnings = true;
+		// UnsafeTypeCastWarningLevel = WarningLevel.Warning;
+
+		PrivateDependencyModuleNames.AddRange(new[]
+		{
+			"Core", "CoreUObject", "Slate", "DeveloperSettings"
+		});
+	}
+}
